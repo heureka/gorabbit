cb879ebf1896dd63b5d3abf91ba29c3333a711f8
Dmytro Kasianenko <36199740+dmksnnk@users.noreply.github.com>
2021-11-11 16:52:29 +0100

    rabbittest: add docs

---
+++ rabbittest/docs.go
@@ -0,0 +1,17 @@
+// Package rabbittest provides "stretchr/testify/suite" testing suite with prepared RabbitMQ connection, channel or topology.
+//
+// Usage:
+//
+//  // embedding ChannelSuite
+//  type TestSuite struct {
+//    rabbittest.ChannelSuite
+//  }
+//
+//  func (s *TestSuite) TestMyFunction() {
+//    s.Channel.Publish(...)
+//  }
+//
+// Set up RABBITMQ_URL environment variable and tun tests:
+//
+//   RABBITMQ_URL=amqp://localhost:5672 go test -v  ./...
+package rabbittest
