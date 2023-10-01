77e8f21541163be2fc5b71d12c7049cce48185f8
Dmytro Kasianenko <kasyanenkodg@gmail.com>
2023-10-01 23:44:43 +0200

    fix(docs): update examples and README

--- examples/one/main.go
+++ examples/one/main.go
@@ -5,23 +5,26 @@ import (
 	"log"
 	"time"
 
-	amqp "github.com/rabbitmq/amqp091-go"
-
 	"github.com/heureka/gorabbit"
+	"github.com/heureka/gorabbit/channel"
+	"github.com/heureka/gorabbit/connection"
 	"github.com/heureka/gorabbit/process"
 )
 
 func main() {
-	conn, err := amqp.Dial("amqp://localhost:5672")
+	// connection with re-dialing capabilities.
+	conn, err := connection.Dial("amqp://localhost:5672")
 	if err != nil {
 		log.Panic(err)
 	}
 
-	consumer, err := gorabbit.NewConsumer(conn, "my-queue")
+	// channel with re-connection capabilities.
+	ch, err := channel.New(conn)
 	if err != nil {
 		log.Panic(err)
 	}
 
+	consumer := gorabbit.NewConsumer(ch, "my-queue")
 	// transaction function for processing single message
 	tx := func(ctx context.Context, msg []byte) error {
 		log.Println(string(msg))
